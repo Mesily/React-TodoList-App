d1a9d002c29eb405d2fb12fa84bc9fe12680f7bb
Mesily <animercy210@gmail.com>
2024-03-18 11:10:14 +0100

    react todo list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function TodoListApp() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [updateIndex, setUpdateIndex] = useState(null);
+  const [updateValue, setUpdateValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      if (updateIndex !== null) {
+        const updatedTodos = [...todos];
+        updatedTodos[updateIndex] = updateValue;
+        setTodos(updatedTodos);
+        setUpdateIndex(null);
+        setUpdateValue('');
+      } else {
+        setTodos([...todos, inputValue]);
+      }
+      setInputValue('');
+    }
+  };
+
+  const handleUpdateTodo = (index) => {
+    const newTask = window.prompt('Enter a new task:', todos[index]);
+    if (newTask !== null) {
+      const updatedTodos = [...todos];
+      updatedTodos[index] = newTask;
+      setTodos(updatedTodos);
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    if (window.confirm('Are you sure you want to delete this task?')) {
+      const newTodos = todos.filter((_, i) => i !== index);
+      setTodos(newTodos);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={updateIndex !== null ? updateValue : inputValue}
+        onChange={handleInputChange}
+        placeholder="Add a new todo"
+      />
+      <button onClick={() => {
+        if (updateIndex !== null) {
+          handleAddTodo();
+          setUpdateIndex(null);
+          setUpdateValue('');
+        } else {
+          handleAddTodo();
+        }
+      }}>
+        {updateIndex !== null ? 'Update' : 'Add'}
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleUpdateTodo(index)}>Update</button>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoListApp;
